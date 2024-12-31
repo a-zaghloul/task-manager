e87fd0f3bac562092a1aac3d5502bfbc8d6ce12f
a-zaghloul <ahmed.mzaghloul@yahoo.com>
2024-12-31 14:16:48 -0500

    develop the base app

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TaskController::class, 'index']);
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::resource('tasks', TaskController::class);
